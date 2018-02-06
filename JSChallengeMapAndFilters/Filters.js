8f8a1534d3bd98f3fb19b73d033eb4e6bfbf75a8
Rakesh Sharma <rakesh.sharma@peopleinteractive.in>
2018-02-06 13:17:16 +0530

    closer and Filter JS file added

---
+++ JSChallengeMapAndFilters/Filters.js
@@ -0,0 +1,47 @@
+/*
+  Follow step by step instructions to complete these 
+  multiple challenges related to arrays.
+*/
+
+/*
+  This require statement conveniently brings in the entire set of user profile
+  data for you from a neighbouring file.
+  
+  You can imagine that this is your network api call delivering you data
+*/
+const profiles = require("./data/shaadiData.json");
+
+/*
+  Challenge 1.1 - write a function that gets only the 
+  user profiles who have never been married
+  Expected const brazilUserProfiles = []
+*/
+
+console.log();
+
+/*
+  Challenge 1.2 - write a function that gets only the 
+  user profiles with college education
+  Parse this piece of education data from the loaded json to return an array 
+  "education": {
+  "education": "High school - Arts",
+  "education_stream": "Arts",
+  "college_university": "",
+  "college_1": "",
+  "valid_college_1": "No",
+  "college_2": "",
+  "valid_college_2": "No"
+}
+  Expected const collegeUserProfiles = []
+*/
+
+console.log();
+
+/*
+  Challenge 1.3 - write a function that gets only the 
+  user profiles with more than one photo
+  Expected const profilesWithMultiplePhotos = [] <-- nat field equals BR
+  
+*/
+
+console.log();
