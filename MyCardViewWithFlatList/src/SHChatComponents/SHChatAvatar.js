6b1ef59418d9b11a66b89b63cbb880229974d6ba
Rakesh Sharma <rakesh.sharma@peopleinteractive.in>
2018-02-15 10:42:10 +0530

    chatcomponents

---
+++ MyCardViewWithFlatList/src/SHChatComponents/SHChatAvatar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+export default class SHChatAvatar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+   getPlaceHolderImage = () => {
+    var icon = this.props.profile.gender == "female" ?
+    require('./Assets/Silhouette_Female.png') :
+    require('./Assets/Silhouette_Male.png')
+    return icon
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+        defaultSource={this.getPlaceHolderImage()} 
+        source = this.props.profile.phot0 />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 30
+  },
+  image: {
+    flex: 1
+  }
+});
